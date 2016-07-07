1c7243e5ad6c7480de12aedc2028489051fa3bb1
Jacob Oakes <jacoboakes92@gmail.com>
2016-07-07 19:42:58 -0400

    Deploying version fe181510888b0aa91d8fb03675def84e76b22614

--- src/Components/GithubLink.elm
+++ src/Components/GithubLink.elm
@@ -1,18 +1,22 @@
 module Components.GithubLink exposing (view)
 
-import Html exposing (Html, img)
-import Html.Attributes exposing (class, src, alt)
+import Html exposing (Html, img, a)
+import Html.Attributes exposing (class, src, alt, title, href)
 import Html.Events exposing (onClick)
 import Analytics
 
 
 view : String -> Html Analytics.Msg
 view imgClasses =
-    img
-        [ class <| "github " ++ imgClasses
-        , src "assets/images/GitHub-Mark-Light-64px.png"
-        , alt "elm-news on github"
-        , onClick <|
-            Analytics.GithubLink "https://github.com/oakesja/elm-news"
+    a
+        [ href "https://github.com/oakesja/elm-news" ]
+        [ img
+            [ class <| "github " ++ imgClasses
+            , src "assets/images/GitHub-Mark-Light-64px.png"
+            , alt "elm-news on github"
+            , title "elm-news on github"
+            , onClick <|
+                Analytics.GithubLink "https://github.com/oakesja/elm-news"
+            ]
+            []
         ]
-        []
